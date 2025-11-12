647d7cdacfe3eaf273dae5b1509256463a387bf1
kr1pton28 <cuentasgeorge13@gmail.com>
2025-11-12 14:52:44 -0500

    Exercise #10 finished

--- ResolucionTaller/script.js
+++ ResolucionTaller/script.js
@@ -93,3 +93,11 @@ console.log(productos);
 let estudiantes = ["Zapata", "Alvarez", "Gomez", "Bernal"];
 estudiantes.sort();
 console.log(estudiantes);
+
+/*
+    Ejercicio 10: Vaciar un arreglo (fill)
+*/
+
+let puntuaciones = [100, 85, 92, 78];
+puntuaciones.fill(0);
+console.log(puntuaciones);
