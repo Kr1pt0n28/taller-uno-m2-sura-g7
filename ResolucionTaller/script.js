0d65b2938b82ede8e3dca28ab083c6dc905d9407
kr1pton28 <cuentasgeorge13@gmail.com>
2025-11-12 14:03:42 -0500

    exercise #1 finished

---
+++ ResolucionTaller/script.js
@@ -0,0 +1,11 @@
+let compras = ["Manzanas"];
+
+compras.push("Leche", "Pan");
+
+console.log(compras)
+
+compras.unshift("Huevos");
+console.log(compras);
+
+compras.splice(1,1);
+console.log(compras);
