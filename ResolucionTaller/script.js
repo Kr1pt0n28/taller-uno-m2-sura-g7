6a1c43d944dbf15030ddb1cb43e917b871090624
kr1pton28 <cuentasgeorge13@gmail.com>
2025-11-12 14:15:23 -0500

    Exercise #2 finished

--- ResolucionTaller/script.js
+++ ResolucionTaller/script.js
@@ -1,7 +1,8 @@
+/*
+    Ejercicio 1: Lista del Súpermercado
+*/
 let compras = ["Manzanas"];
-
 compras.push("Leche", "Pan");
-
 console.log(compras)
 
 compras.unshift("Huevos");
@@ -9,3 +10,20 @@ console.log(compras);
 
 compras.splice(1,1);
 console.log(compras);
+
+/*
+    Ejercicio 2: Pila de Tareas Pendientes (LIFO)
+*/
+
+let tareas = ["Revisar emails"];
+tareas.unshift("Llamar al cliente");
+console.log(tareas);
+
+tareas.unshift("Preparar reporte");
+console.log(tareas);
+
+tareas.splice(0,1);
+console.log(tareas);
+tareas.splice(0,1);
+console.log(tareas);
+
